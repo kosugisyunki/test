b11363e31b9901b1f62592ef8846ea573a975d67
kosugisyunki <syunki1103@gmail.com>
2018-12-19 21:34:12 +0900

    12/19

--- Person/src/jp/co/internous/action/Test.java
+++ Person/src/jp/co/internous/action/Test.java
@@ -5,56 +5,27 @@ public class Test {
 
 	public static void main(String[] args){
 
-		Person ziro=new Person();
-
-		ziro.name="木村次郎";
-		ziro.age=18;
-		ziro.phoneNumber="012-345-6789";
-		ziro.address="abc@de.jp";
 
 
-
-		Person hanako=new Person();
-
-		hanako.name="鈴木花子";
-		hanako.age=16;
-		hanako.phoneNumber="000-0000-0000";
-		hanako.address="adadkk@jiji.jp";
+		Robot aibo=new Robot();
+		aibo.name="アイボ";
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
 
 
-
-		Person kosugi=new Person();
-
-		kosugi.name="小杉駿希";
-		kosugi.age=27;
-		kosugi.phoneNumber="1212-1212-1212";
-		kosugi.address="syunki";
+		Robot asimo=new Robot();
+		asimo.name="アシモ";
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
 
 
-
-
-		System.out.print(ziro.name+"、");
-		System.out.println(ziro.age);
-		ziro.talk();
-		ziro.walk();
-		ziro.run();
-
-
-		System.out.print(hanako.name+"、");
-		System.out.println(hanako.age);
-		hanako.talk();
-		hanako.walk();
-		hanako.run();
-
-
-		System.out.print(kosugi.name+"、");
-		System.out.println(kosugi.age);
-		kosugi.talk();
-		kosugi.walk();
-		kosugi.run();
-
-
-
+		Robot pepper=new Robot();
+		pepper.name="ペッパー";
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
 
 
 	}
